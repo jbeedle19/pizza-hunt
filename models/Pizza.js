a527dfae7003f27d177036c4924709ebda8d6e77
jbeedle19 <josh.beedle@gmail.com>
2021-03-01 16:57:33 -0500

    created Comment model

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,22 +1,41 @@
 const { Schema, model } = require('mongoose');
 
 // Create the schema for the Pizza model
-const PizzaSchema = new Schema({
-    pizzaName: {
-        type: String,
+const PizzaSchema = new Schema(
+    {
+        pizzaName: {
+            type: String,
+        },
+        createdBy: {
+            type: String,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        size: {
+            type: String,
+            default: 'Large'
+        },
+        toppings: [],
+        comments: [
+            {
+                type: Schma.Types.ObjectId,
+                ref: 'Comment'
+            }
+        ]
     },
-    createdBy: {
-        type: String,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    size: {
-        type: String,
-        default: 'Large'
-    },
-    toppings: []
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+// Get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
 });
 
 // Create the Pizza model using the PizzaSchema
